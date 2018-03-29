f9caf953906ce8503cc3c89fe42bf990b37e26c6
Purvi <Purvi@PURVI>
2018-03-29 06:17:16 +0530

    updated

---
+++ MyNewMaven/src/test/java/SampleTest/SampleClass.java
@@ -0,0 +1,17 @@
+package SampleTest;
+
+import org.testng.annotations.Test;
+
+public class SampleClass {
+ 
+	@Test
+	public void Sample()
+	{
+		System.out.println("This is a sample program");
+	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+
+}
---
+++ MyNewMaven/src/test/java/SampleTest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Purvi
+ *
+ */
+package SampleTest;
