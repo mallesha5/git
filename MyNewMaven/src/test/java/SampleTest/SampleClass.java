46af622d37edce15fa139edb73361ff7ebf0fd18
Purvi <Purvi@PURVI>
2018-03-29 20:34:43 +0530

    updated

--- MyNewMaven/src/test/java/SampleTest/SampleClass.java
+++ MyNewMaven/src/test/java/SampleTest/SampleClass.java
@@ -9,8 +9,7 @@ public class SampleClass {
 	{
 		System.out.println("This is a sample program");
 	}
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
+
 //
 //	}
 
